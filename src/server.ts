6c0e5e067a55ad6b231289ededd3e95664e08b3b
sujanchakma1 <sujanckz926@gmail.com>
2025-12-02 14:00:48 +0600

    added user post, get all user & get single user

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import path from "path";
 const app = express();
 const port = 5000;
-dotenv.config({path: path.join(process.cwd(), '.env')})
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 //* Parser
 app.use(express.json());
-// app.use(express.urlencoded())
+app.use(express.urlencoded());
 
 const pool = new Pool({
   connectionString: `${process.env.CONNECTION_STR}`,
@@ -28,7 +28,7 @@ const initDB = async () => {
     )
     `);
 
-    await pool.query(`
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS todos(
       id SERIAL PRIMARY KEY,
       user_id INT REFERENCES users(id) ON DELETE CASCADE,
@@ -39,21 +39,79 @@ const initDB = async () => {
       created_at TIMESTAMP DEFAULT NOW(),
       updated_at TIMESTAMP DEFAULT NOW()
       )
-      `)
+      `);
 };
 
-initDB();
+// initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next Level Developer!");
 });
 
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
-  res.status(201).json({
-    success: true,
-    message: "Api is Working",
-  });
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+    console.log(result.rows[0]);
+    res.status(201).json({
+      success: true,
+      message: "data inserted successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      message: "Users fetch successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+});
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  // res.send({message: "API is cool....."})
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User found successfully",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 app.listen(port, () => {
